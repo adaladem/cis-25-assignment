b429bfd5a52902e8b96ac8c4ea5425d17e92109a
adal adem <adulihakim1@gmail.com>
2026-01-28 16:03:46 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+int main() {
+int points;
+std::string name;
+points = 100;
+name = "Adal";
+std::cout << "Hello, " << name 
+          << "! The computer has seen fit to assign you " 
+          << points << " points." << std::endl;
+return 0;
+}
